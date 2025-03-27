2a25feaa43d69f85d9b6e9b19a3bc4e208f36005
Rasmus <rame0001@stud.kea.dk>
2025-03-27 15:13:06 +0100

    Enhance Hero component layout and styling; add new images for visual appeal

--- iwatch/src/app/components/Hero.jsx
+++ iwatch/src/app/components/Hero.jsx
@@ -2,7 +2,27 @@
 
 export default function Hero() {
   return (
-      <section>
+      <section className="hero">
+        <div className="hero-content">
+          <div className="hero-grid1">
+            <h1>The Perfect Moment</h1>
+            <h2>Between Past and Future</h2>
+            <button>Buy Now</button>
+            <p>← 1 →</p>
+          </div>
+          <div className="hero-grid2">
+            <img src="/images/watch.png" alt="Watch" />
+            <img src="/images/ColorIndicator.png" alt="Watch" />
+          </div>
+            <div className="three-watches">
+              <span></span>
+              <span></span>
+              <span></span>
+              <img src="/images/ur1.png" alt="small-watch-black" />
+              <img src="/images/ur2.png" alt="small-watch-green" />
+              <img src="/images/ur3.png" alt="small-watch-pink" />
+            </div>
+        </div>
       </section>
   );
 }
