01e9e1656e0877f4947035341d34ca15355ea9f2
Rasmus <rame0001@stud.kea.dk>
2025-03-28 14:55:29 +0100

    done med layout

--- iwatch/src/app/components/Hero.jsx
+++ iwatch/src/app/components/Hero.jsx
@@ -12,7 +12,13 @@ export default function Hero() {
           </div>
           <div className="hero-grid2">
             <img src="/images/watch.png" alt="Watch" />
-            <img src="/images/ColorIndicator.png" alt="Watch" />
+            <div className="cirkel-indicator">
+              <span></span>
+              <div className="line"></div>
+              <span></span>
+              <div className="line"></div>
+              <span></span>
+            </div>
           </div>
         </div>
           <ThreeWatches />
