bf6c047e97ab051356b304d9187c0fe97b6bc03b
RasmusMeinche <rame0001@stud.kea.dk>
2025-04-02 16:59:57 +0200

    blah

--- iwatch/src/app/components/Hero.jsx
+++ iwatch/src/app/components/Hero.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+/* import { useState } from "react"; */
 import ThreeDots from "./ThreeDots";
 import ThreeWatches from "./ThreeWatches";
 
 
-const Hero = () => {
-  const [setMintWatch] = useState(true);
+function Hero () {
+
   return (
       <section className="hero">
         <div className="hero-content">
