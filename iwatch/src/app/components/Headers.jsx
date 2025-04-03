5d6ba08c08aade39f0dce48445a15a59f6d46afd
Rasmus <rame0001@stud.kea.dk>
2025-04-03 15:32:39 +0200

    Enhance navigation and color selection in Headers and ThreeWatches components

--- iwatch/src/app/components/Headers.jsx
+++ iwatch/src/app/components/Headers.jsx
@@ -1,8 +1,14 @@
+"use client"
+
 import { FiSearch } from "react-icons/fi";
 import { LuShoppingBag } from "react-icons/lu";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Headers() {
+
+const activePath = usePathname();
+
   return (
    
     <header className="bg-[#B6CCDA] text-white grid grid-cols-[minmax(20px,0.2fr)_1fr_minmax(20px,0.2fr)] pt-2 pb-2">
@@ -10,12 +16,12 @@ export default function Headers() {
         <div>
           <img src="/images/Logo.png" alt="" />
         </div>
-        <div className="flex gap-6">
-          <a>Mac</a>
-          <a>Iphone</a>
-          <a>Ipad</a>
-          <Link href="/iwatch">iWatch</Link>
-          <a>Support</a>
+        <div className="flex gap-6 items-center">
+          <Link className={`${activePath === "/imac" ? "bg-white text-[#B6CCDA] rounded-full px-4 py-1" : ""}`} href="/imac">iMac</Link>
+          <Link className={`${activePath === "/iphone" ? "bg-white text-[#B6CCDA] rounded-full px-4 py-1" : ""}`} href="/iphone">iPhone</Link>
+          <Link className={`${activePath === "/ipad" ? "bg-white text-[#B6CCDA] rounded-full px-4 py-1" : ""}`} href="/ipad">iPad</Link>
+          <Link className={`${activePath === "/iwatch" ? "bg-white text-[#B6CCDA] rounded-full px-4 py-1" : ""}`} href="/iwatch">iWatch</Link>
+          <Link href="/support">iSupport</Link>
         </div>
         <div className="flex items-center gap-3">
         <FiSearch className="searh-icon"/>
--- iwatch/src/app/components/ThreeWatches.jsx
+++ iwatch/src/app/components/ThreeWatches.jsx
@@ -6,10 +6,13 @@ export default function ThreeWatches({ onColorChange }) {
       <div>
         <p className="left">← 1 →</p>
       </div>
-      <div className="flex justify-end gap-16 items-center">
-        <img src="/images/ur1.png" alt="navy" className="cursor-pointer w-20" onClick={() => onColorChange("navy")} />
-        <img src="/images/ur2.png" alt="mint" className="cursor-pointer w-20" onClick={() => onColorChange("mint")} />
-        <img src="/images/ur3.png" alt="ocean" className="cursor-pointer w-20" onClick={() => onColorChange("ocean")} />
+      <div className="flex justify-end gap-16 items-center relative">
+      <span className="absolute w-[120px] h-[70px] rounded-[15px] top-8 left-[10rem] bg-[#434558]" ></span>
+      <span className="absolute w-[120px] h-[70px] rounded-[15px] top-8 left-[19rem] bg-[#6ADDCC]"></span>
+      <span className="absolute w-[120px] h-[70px] rounded-[15px] top-8 left-[28rem] bg-[#c0daea;]"></span>
+        <img src="/images/ur1.png" alt="navy" className="cursor-pointer w-20 z-1" onClick={() => onColorChange("navy")} />
+        <img src="/images/ur2.png" alt="mint" className="cursor-pointer w-20 z-1" onClick={() => onColorChange("mint")} />
+        <img src="/images/ur3.png" alt="ocean" className="cursor-pointer w-20 z-1" onClick={() => onColorChange("ocean")} />
       </div>
     </div>
   );
